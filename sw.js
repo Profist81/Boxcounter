09f7ddf6337b1273989dee0ab91e88acc9109984
Profist81 <profist19@googlemail.com>
2025-09-21 10:51:12 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bcm-v38';
+const CACHE='bcm-v39';
 const CORE=['./','index.html','manifest.webmanifest','icons/icon-192.png','icons/icon-512.png'];
 self.addEventListener('install',e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
